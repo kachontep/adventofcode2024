26875c73b35f7276767686ad78c95e53224b729d
Kajornthep Piyanun <kajornthep.p@linecorp.com>
2024-12-10 14:34:32 +0700

    Complate day9 part2

--- day9/disk_framenter.py
+++ day9/disk_framenter.py
@@ -1,3 +1,4 @@
+from os import read
 import sys
 from functools import reduce
 from collections import namedtuple
@@ -7,7 +8,7 @@ SPACE_ID = -1
 Block = namedtuple("Block", "id size")
 
 
-def read_blocks(SPACE_ID, Block) -> list[Block]:
+def read_blocks() -> list[Block]:
     return [
         Block(SPACE_ID, int(c)) if i % 2 == 1 else Block(i // 2, int(c))
         for i, c in enumerate(sys.stdin.read())
@@ -16,22 +17,23 @@ def read_blocks(SPACE_ID, Block) -> list[Block]:
 
 def checksum(blocks: list[Block]) -> int:
     def block_sum(start_pos: int, total: int, b: Block) -> tuple[int, int]:
-        return start_pos + b.size, total + sum(
-            i * b.id for i in range(start_pos, start_pos + b.size)
-        )
+        block_total = 0
+        if b.id != SPACE_ID:
+            block_total = sum(i * b.id for i in range(start_pos, start_pos + b.size))
+        return start_pos + b.size, total + block_total
 
     _, result = reduce(
         lambda c, b: block_sum(c[0], c[1], b),
-        blocks[:-1],
+        blocks,
         (0, 0),
     )
     return result
 
 
-def compressed_blocks(blocks: list[Block]) -> list[Block]:
+def compress_blocks_part1(blocks: list[Block]) -> list[Block]:
     compressed = []
     left, right = 0, len(blocks) - 1
-    compressed_space = 0
+    total_space = 0
 
     while left <= right:
         left_block, right_block = blocks[left], blocks[right]
@@ -50,37 +52,95 @@ def compressed_blocks(blocks: list[Block]) -> list[Block]:
 
         if space_left == space_used:
             compressed.append(right_block)
-            compressed_space += space_left
+            total_space += space_left
             right -= 1
             left += 1
         elif space_left > space_used:
             compressed.append(right_block)
             blocks[left] = Block(SPACE_ID, space_left - space_used)
-            compressed_space += space_used
+            total_space += space_used
             right -= 1
         elif space_left < space_used:
             new_block = Block(block_id, space_left)
             remain_block = Block(block_id, space_used - space_left)
-            compressed_space += space_left
+            total_space += space_left
             compressed.append(new_block)
             blocks[right] = remain_block
             left += 1
 
-    if compressed_space > 0:
-        compressed.append(Block(SPACE_ID, compressed_space))
+    if total_space > 0:
+        compressed.append(Block(SPACE_ID, total_space))
 
     return compressed
 
 
+def compress_blocks_part2(blocks: list[Block]) -> list[Block]:
+    done = False
+    result = blocks[:]
+
+    while not done:
+
+        moves = 0
+        left = 0
+        right = len(result) - 1
+        subblocks_left = []
+        subblocks_right = []
+
+        while left <= right:
+            left_block, right_block = result[left], result[right]
+
+            if left_block.id != SPACE_ID:
+                subblocks_left.append(left_block)
+                left += 1
+                continue
+
+            if right_block.id == SPACE_ID:
+                subblocks_right.append(right_block)
+                right -= 1
+                continue
+
+            _, space_left = left_block
+            block_id, space_used = right_block
+
+            if space_left >= space_used:
+                subblocks_right.append(Block(SPACE_ID, space_used))
+
+                subblocks_left.append(Block(block_id, space_used))
+                if space_left > space_used:
+                    subblocks_left.append(Block(SPACE_ID, space_left - space_used))
+
+                moves += 1
+                left += 1
+                right -= 1
+            else:
+                subblocks_right.append(right_block)
+                right -= 1
+
+        result = subblocks_left + subblocks_right
+
+        if moves == 0:
+            done = True
+            
+    return result
+
+
 def solve_part1():
-    blocks = read_blocks(SPACE_ID, Block)
-    blocks = compressed_blocks(blocks)
+    blocks = read_blocks()
+    blocks = compress_blocks_part1(blocks)
     answer = checksum(blocks)
     print(answer)
 
 
+def solve_part2():
+    blocks = read_blocks()
+    blocks = compress_blocks_part2(blocks)
+    print(blocks)
+    answer = checksum(blocks)
+    print(answer)
+
 def main():
-    solve_part1()
+    # solve_part1()
+    solve_part2()
 
 
 if __name__ == "__main__":
