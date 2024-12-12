f44141199c43196ae626b62ef6a4c31bc47cf130
Kajornthep Piyanun <kajornthep.p@linecorp.com>
2024-12-12 19:03:51 +0700

    Refactor day11 part1

--- day11/plutonian_pebble.py
+++ day11/plutonian_pebble.py
@@ -6,16 +6,14 @@ def main():
     stones = list(map(int, sys.stdin.read().split()))
     print(f"stones: {stones!r}")
 
-    # new_stones = solve_part_1(stones)
-    # print(f"#new_stones: {len(new_stones)!r}")
-
-    total_stones = solve_part_2(stones)
-    print(f"#new_stones: {total_stones}")
+    total_stones = solve_part_1(stones)
+    # total_stones = solve_part_2(stones)
+    print(f"#num_stones: {total_stones}")
 
 
 
-def solve_part_1(stones: list[int]) -> list[int]:
-    return blink_part_1(stones, times=25)
+def solve_part_1(stones: list[int]) -> int:
+    return sum(stones_count(s, 25) for s in stones)
 
 
 def solve_part_2(stones: list[int]) -> int:
@@ -37,32 +35,6 @@ def stones_count(x: int, n: int, t: int = 0, c: int = 1) -> int:
         )
     else:
         return stones_count(x * 2024, n, t + 1, c)
-
-
-def blink_part_1(stones, times):
-    @lru_cache(maxsize=None)
-    def split(s: int) -> list[int]:
-        result = []
-        ss = str(s)
-        if len(ss) % 2 == 0:
-            mid = len(ss) // 2
-            result.append(int(ss[:mid]))
-            result.append(int(ss[mid:]))
-        return result
-
-    for _ in range(times):
-        new_stones = []
-        for s in stones:
-            if s == 0:
-                new_stones.append(1)
-                continue
-            if ss := split(s):
-                new_stones.extend(ss)
-                continue
-            new_stones.append(s * 2024)
-        stones = new_stones
-    return new_stones
-
 
 if __name__ == "__main__":
     main()
