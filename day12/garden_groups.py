18e19208ff14991d507de1fbf6799aaaae9611e9
Kajornthep Piyanun <kajornthep.p@linecorp.com>
2024-12-15 17:35:06 +0700

    Complete day12 part2

--- day12/garden_groups.py
+++ day12/garden_groups.py
@@ -18,8 +18,7 @@ def survey(
     r: int,
     c: int,
     table: list[list[str]],
-    border: bool = False,
-) -> tuple[int, int, set[tuple[int, int]]]:
+) -> tuple[int, list[tuple[int, int, int]], set[tuple[int, int]]]:
     max_row = len(table)
     max_col = len(table[0])
 
@@ -37,47 +36,71 @@ def survey(
             return 0, [(r, c, direction)], set()
 
         area = 1
-        perimeter = []
+        perim = []
         region = region | {(r, c)}
 
         for di, (dr, dc) in enumerate([(-1, 0), (0, 1), (1, 0), (0, -1)], start=1):
-            n_area, n_perimeter, n_region = measure(r + dr, c + dc, s, region, di)
+            n_area, n_perim, n_region = measure(r + dr, c + dc, s, region, di)
             area += n_area
-            perimeter += n_perimeter
+            perim += n_perim
             region |= n_region
 
-        return area, perimeter, region
+        return area, perim, region
 
-    area, perimeters, region = measure(r, c, table[r][c], set())
-
-    if not border:
-        return area, len(perimeters), region
-    
-    borders = perimeters[:]
-    border_ctr = 0
-    print(f"{borders}")
-    while borders:
-        print(f"{borders[0]}")
-        bfp = _border_from_perimeter(borders[0], perimeters=borders)
-        print(f"{bfp}")
-        for b in bfp:
-            borders.remove(b)
-        border_ctr += 1
+    area, perims, region = measure(r, c, table[r][c], set())
+    return area, perims, region
 
 
-    return area, border_ctr, region
+def num_borders_from_perims(perims: list[tuple[int, int, int]]) -> int:
+    def neighbor_perims(p: tuple[int, int, int]) -> set[tuple[int, int, int]]:
+        return {
+            (p[0] + dr, p[1] + dc, p[2])
+            for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        }
 
-def _border_from_perimeter(p: tuple[int, int, int], perimeters: list[tuple[int, int, int]], border: set[tuple[int, int, int]]|None = None) -> set[tuple[int, int, int]]:
-    border = border or {p}
-    for pp in perimeters:
-        if pp in border:
-            continue
-        for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            if (p[0] + dr, p[1] + dc, p[2]) == pp:
-                border |= _border_from_perimeter(pp, perimeters, border | {p})
-    return border
+    relations: list[tuple[int, int]] = []
+    for i, p in enumerate(perims):
+        nps = neighbor_perims(p)
+        for j, q in enumerate(perims):
+            if q in nps:
+                relations.append((i, j))
 
-def main():
+    borders = [i for i, _ in enumerate(perims)]
+    for i, j in relations:
+        i, j = min(i, j), max(i, j)
+        for k in range(len(borders)):
+            if borders[k] == borders[j]:
+                borders[k] = borders[i]
+
+    return len(set(borders))
+
+
+def solve_part1():
+    table = [[c for c in list(line.rstrip())] for line in sys.stdin.readlines()]
+    # show_map(table)
+
+    marked = [[False for _ in row] for row in table]
+
+    total_cost = 0
+
+    for r in range(len(table)):
+        for c in range(len(table[r])):
+            if marked[r][c]:
+                continue
+
+            area, perims, region = survey(r, c, table)  # Part 1
+            perims_num = len(perims)
+
+            # show_info(table[r][c], area, perims_num, region)
+            total_cost += area * perims_num
+
+            for mr, mc in region:
+                marked[mr][mc] = True
+
+    print(total_cost)
+
+
+def solve_part2():
     table = [[c for c in list(line.rstrip())] for line in sys.stdin.readlines()]
     show_map(table)
 
@@ -90,16 +113,21 @@ def main():
             if marked[r][c]:
                 continue
 
-            # area, perimeter, region = survey(r, c, table, use_border=False) # Part1
-            area, perim_or_border, region = survey(r, c, table, border=True)  # Part 2
+            area, perims, region = survey(r, c, table)  # Part 2
+            borders_num = num_borders_from_perims(perims)
 
-            show_info(table[r][c], area, perim_or_border, region)
-            total_cost += area * perim_or_border
+            show_info(table[r][c], area, borders_num, region)
+            total_cost += area * borders_num
 
             for mr, mc in region:
                 marked[mr][mc] = True
 
     print(total_cost)
+
+
+def main():
+    # solve_part1()
+    solve_part2()
 
 
 if __name__ == "__main__":
