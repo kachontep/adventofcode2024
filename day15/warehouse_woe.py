d5ad3fb38ed8eef9469d46c654d1cbb78654205c
Kajornthep Piyanun <kajornthep.p@linecorp.com>
2024-12-16 22:43:46 +0700

    Complete day15 part 1 and part 2

--- day15/warehouse_woe.py
+++ day15/warehouse_woe.py
@@ -1,17 +1,24 @@
+from shutil import move
 import sys
 
 World = list[list[str]]
+Vector2 = tuple[int, int]
 
 ROBOT = "@"
 BLOCK = "#"
 WALK_PATH = "."
-WAREHOUSE = "O"
-DOUBLE_WAREHOUSE = "[]"
+WAREHOUSE1 = "O"
+WAREHOUSE2 = "[]"
 
 UP = "^"
 RIGHT = ">"
 DOWN = "v"
 LEFT = "<"
+
+UP_VEC2 = (0, -1)
+RIGHT_VEC2 = (1, 0)
+DOWN_VEC2 = (0, 1)
+LEFT_VEC2 = (-1, 0)
 
 
 def show_world(world: World) -> None:
@@ -43,7 +50,9 @@ def world_and_controls():
 
 
 def main():
-    solve_part1()
+    # solve_part1()
+    solve_part2()
+
 
 def solve_part1():
     world, controls = world_and_controls()
@@ -51,20 +60,39 @@ def solve_part1():
 
     robot, *_ = robot_position(world)
 
-    for c in controls:
-        robot = send_control(robot, c, world)
+    for seq_no, c in enumerate(controls):
+        robot = send_control(robot, c, world, seq_no)
 
     show_world(world)
 
-    answer = gps_coordinates(world)
+    answer = gps_coordinates(world, warehouse_type=WAREHOUSE1)
     print(answer)
 
-def gps_coordinates(world):
+
+def solve_part2():
+    world, controls = world_and_controls()
+    world = new_world(world)
+    show_world(world)
+
+    robot, *_ = robot_position(world)
+
+    for seq_no, c in enumerate(controls):
+        robot = send_control(robot, c, world, seq_no)
+        # show_world(world)
+
+    show_world(world)
+
+    answer = gps_coordinates(world, warehouse_type=WAREHOUSE2)
+    print(answer)
+
+
+def gps_coordinates(world, warehouse_type: str):
+    w = warehouse_type if warehouse_type == WAREHOUSE1 else WAREHOUSE2[0]
     result = 0
     for y, r in enumerate(world):
         for x, c in enumerate(r):
-            if c == WAREHOUSE:
-                result += y*100 + x
+            if c == w:
+                result += y * 100 + x
     return result
 
 
@@ -72,72 +100,153 @@ def robot_position(world):
     return [(x, y) for y, r in enumerate(world) for x, c in enumerate(r) if c == ROBOT]
 
 
-def send_control(robot: tuple[int, int], control: str, world: World) -> tuple[int, int]:
+def send_control(
+    robot: tuple[int, int], control: str, world: World, seq_no: int
+) -> tuple[int, int]:
     mv = move_vec(control)
     x, y = robot
     nx, ny = x + mv[0], y + mv[1]
 
-    # print((x, y), control, (nx, ny), "world[ny][nx]: ", world[ny][nx])
+    # print(f"{seq_no}:", (x, y), control, (nx, ny), "world[ny][nx]: ", world[ny][nx])
 
     if world[ny][nx] == BLOCK:
         return robot
 
-    if world[ny][nx] == WALK_PATH:
+    elif world[ny][nx] == WALK_PATH:
         world[y][x], world[ny][nx] = WALK_PATH, ROBOT
         robot = nx, ny
 
-    if world[ny][nx] == WAREHOUSE:
+    elif world[ny][nx] == WAREHOUSE1:
         if warehouse_movable((nx, ny), mv, world):
             move_warehouse((nx, ny), mv, world)
+            world[y][x] = WALK_PATH
+            world[ny][nx] = ROBOT
+            robot = nx, ny
+    elif is_warehouse2((nx, ny), world):
+        if warehouse_movable((nx, ny), mv, world):
+            move_warehouse2((nx, ny), mv, world)
             world[y][x] = WALK_PATH
             world[ny][nx] = ROBOT
             robot = nx, ny
 
     return robot
 
-def warehouse_movable(
-    position: tuple[int, int], move_vec: tuple[int, int], world: World
-) -> bool:
-    x, y = position
+
+def is_warehouse2(pos: Vector2, world: World) -> bool:
+    return world[pos[1]][pos[0]] in set(list(WAREHOUSE2))
+
+
+def warehouse2_components(pos: Vector2, world: World) -> tuple[Vector2, Vector2]:
+    nx, ny = pos
+    if world[ny][nx] == WAREHOUSE2[0]:
+        return (nx, ny), (nx + 1, ny)
+    elif world[ny][nx] == WAREHOUSE2[1]:
+        return (nx - 1, ny), (nx, ny)
+    else:
+        raise ValueError(f"({nx}, {ny}) doesn't contain a double warehouse")
+
+
+def warehouse_movable(pos: Vector2, mv: tuple[int, int], world: World) -> bool:
+    x, y = pos
+
     if world[y][x] == BLOCK:
         return False
     if world[y][x] == WALK_PATH:
         return True
 
-    nx, ny = x + move_vec[0], y + move_vec[1]
-    return warehouse_movable((nx, ny), move_vec, world)
+    if is_warehouse2((x, y), world):
+        c1, c2 = warehouse2_components((x, y), world)
+        nc1 = next_move(c1, mv)
+        nc2 = next_move(c2, mv)
+
+        if mv == UP_VEC2 or mv == DOWN_VEC2:
+            c1_mv = warehouse_movable(nc1, mv, world)
+            c2_mv = warehouse_movable(nc2, mv, world)
+            return c1_mv and c2_mv
+        elif mv == LEFT_VEC2:
+            return warehouse_movable(nc1, mv, world)
+        elif mv == RIGHT_VEC2:
+            return warehouse_movable(nc2, mv, world)
+
+    return warehouse_movable(next_move(pos, mv), mv, world)
 
 
-def move_warehouse(
-    position: tuple[int, int], move_vec: tuple[int, int], world: World
-) -> None:
-    x, y = position
-    nx, ny = x + move_vec[0], y + move_vec[1]
-
-    if world[y][x] == WAREHOUSE and world[ny][nx] == WAREHOUSE:
-        move_warehouse((nx, ny), move_vec, world)
-
-    world[ny][nx] = WAREHOUSE
-    world[y][x] = WALK_PATH
+def next_move(pos: Vector2, mv: Vector2) -> Vector2:
+    return pos[0] + mv[0], pos[1] + mv[1]
 
 
-def move_double_warehouse():
-    # TODO:
-    pass
+def move_warehouse(pos: tuple[int, int], mv: tuple[int, int], world: World) -> None:
+    x, y = pos
+    nx, ny = x + mv[0], y + mv[1]
 
-def move_vec(control):
+    if world[y][x] == WAREHOUSE1 and world[ny][nx] == WAREHOUSE1:
+        move_warehouse((nx, ny), mv, world)
+
+    world[y][x], world[ny][nx] = WALK_PATH, world[y][x]
+
+
+def move_warehouse2(pos: tuple[int, int], mv: tuple[int, int], world: World) -> None:
+    if not is_warehouse2(pos, world):
+        return
+
+    c1, c2 = warehouse2_components(pos, world)
+    nc1 = next_move(c1, mv)
+    nc2 = next_move(c2, mv)
+
+    if mv == UP_VEC2 or mv == DOWN_VEC2:
+        move_warehouse2(nc1, mv, world)
+        move_warehouse2(nc2, mv, world)
+
+        world[nc1[1]][nc1[0]], world[nc2[1]][nc2[0]] = WAREHOUSE2[0], WAREHOUSE2[1]
+        world[c1[1]][c1[0]], world[c2[1]][c2[0]] = WALK_PATH, WALK_PATH
+
+    elif mv == LEFT_VEC2:
+        move_warehouse2(nc1, mv, world)
+        world[nc1[1]][nc1[0]], world[c1[1]][c1[0]], world[c2[1]][c2[0]] = (
+            WAREHOUSE2[0],
+            WAREHOUSE2[1],
+            WALK_PATH,
+        )
+
+    elif mv == RIGHT_VEC2:
+        move_warehouse2(nc2, mv, world)
+        world[nc2[1]][nc2[0]], world[c2[1]][c2[0]], world[c1[1]][c1[0]] = (
+            WAREHOUSE2[1],
+            WAREHOUSE2[0],
+            WALK_PATH,
+        )
+
+
+def move_vec(control: str) -> Vector2:
     match control:
         case "^":
-            move_vec = (0, -1)
+            move_vec = UP_VEC2
         case ">":
-            move_vec = (1, 0)
+            move_vec = RIGHT_VEC2
         case "v":
-            move_vec = (0, 1)
+            move_vec = DOWN_VEC2
         case "<":
-            move_vec = (-1, 0)
+            move_vec = LEFT_VEC2
         case _:
             move_vec = (0, 0)
     return move_vec
+
+
+def new_world(world: World) -> World:
+    def world_change(c: str) -> str:
+        match c:
+            case "@":
+                return "@."
+            case "#":
+                return "##"
+            case "O":
+                return "[]"
+            case ".":
+                return ".."
+            case _:
+                raise ValueError(f"Unknown '{c}' character found")
+
+    return [list("".join(map(world_change, r))) for r in world]
 
 
 if __name__ == "__main__":
